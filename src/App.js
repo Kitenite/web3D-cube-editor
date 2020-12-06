7d5038f321e97b70a984dd1be2501dfcebe39893
Kiet Ho <hoakiet98@gmail.com>
2020-12-06 15:42:56 -0600

    some controls

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Web3D from './Web3D/Web3D'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>This is the header</h1>
-      </header>
       <Web3D></Web3D>
     </div>
   );
